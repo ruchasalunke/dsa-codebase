608d9ad95c8cbeb67d94f9c0f9f0f9b792ac4fe5
Rucha Salunke <76227507+ruchasalunke@users.noreply.github.com>
2025-03-16 00:41:58 +0530

    Create Check If The String Is A Palindrome.cpp
    You are given a string 'S'. Your task is to check whether the string is palindrome or not. For checking palindrome, consider alphabets and numbers only and ignore the symbols and whitespaces.
    
    Note :
    
    String 'S' is NOT case sensitive.
    Example :
    
    Let S = “c1 O$d@eeD o1c”.
    If we ignore the special characters, whitespaces and convert all uppercase letters to lowercase, we get S = “c1odeedo1c”, which is a palindrome. Hence, the given string is also a palindrome.
    
    https://www.naukri.com/code360/problems/check-if-the-string-is-a-palindrome_1062633?

---
+++ Check If The String Is A Palindrome.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+bool checkPalindrome(string s)
+{
+    int i=0;
+    int j=s.length()-1;
+    while(i<j) 
+    {
+        if(!isalnum(s[i])) 
+        {
+            i++;
+            continue;
+        }
+        if(!isalnum(s[j])) 
+        {
+            j--;
+            continue;
+        }
+        if(tolower(s[i]) != tolower(s[j]))
+            return false;
+        i++;
+        j--;
+    }
+    return true;
+}
